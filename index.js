e921c9dd41f70ee09bdfc8c21210a0aa3d5a6832
Sarah Currey <curreysarah28@gmail.com>
2018-05-04 00:14:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,5 +17,13 @@ return ('Currently serving ${katzDeli.shift()}.');
 }
  var line=[] ;
 function currentLine (katzDeli) {
-  
-} 
+ let i = 0;
+  while (i < katzDeli.length) {
+   line.push(` `+[i+1]+`. `  + katzDeli[i])
+    i++;
+  }
+ if (katzDeli.length === 0) {
+   return "The line is currently empty.";
+ } else
+  return(`The line is currently:` + line);
+}
