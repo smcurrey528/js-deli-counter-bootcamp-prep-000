65d99df8083d2f3fc4bb2c6e6835ebbf3a6315d5
Sarah Currey <curreysarah28@gmail.com>
2018-05-04 00:16:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,14 +16,15 @@ else
 return ('Currently serving ${katzDeli.shift()}.');
 }
  var line=[] ;
-function currentLine (katzDeli) {
- let i = 0;
-  while (i < katzDeli.length) {
-   line.push(` `+[i+1]+`. `  + katzDeli[i])
-    i++;
-  }
- if (katzDeli.length === 0) {
-   return "The line is currently empty.";
- } else
-  return(`The line is currently:` + line);
-}
+function currentLine(katzDeliLine){
+  var str = `The line is currently: `;
+  var newLine = [];
+  if (katzDeliLine.length === 0) return "The line is currently empty.";
+  else{
+    for (let i = 0; i < katzDeliLine.length; i++){
+        newLine[i] = `${[i+1]}. ${katzDeliLine[i]}`;
+      }
+    str += newLine.join(', ')
++    return str;
++  }
++}
