af64028fc18075235de95a4d656612913de3ca7c
Sarah Currey <curreysarah28@gmail.com>
2018-05-04 00:05:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var katzDeli= [];
 function takeANumber (katzDeliLine, name) {
   katzDeli.push('${name}');
   return 'Welcome ${name}.You are number ${katzDeli.length} in line');
+  return
 }
 
 function nowServing () {
